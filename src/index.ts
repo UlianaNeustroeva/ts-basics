86c47fa1dda85d9ec01eac56e1d9f206c1b56dd4
Ulya <uliana.neustroeva@nimbledevices.com>
2023-02-21 13:52:18 +0200

    Add arrays

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-let age: number = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+//Types
+let sales = 123_456_789;
+let coutse = 'TypeScript';
+let is_published = true;
+let level;
+
+function render(document: any) {
+    console.log(document);
+
+}
+
+//Arrays
+let numbers = [1, 2, 3];
+numbers.forEach(n => n.toExponential);
+
+//Tuples
+let user: [number, string] = [20, 'Ulya'];
