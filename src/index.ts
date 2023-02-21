5efc87872ecf667c46daa9b9c116338323a115b2
Ulya <uliana.neustroeva@nimbledevices.com>
2023-02-21 15:41:23 +0200

    Working with an object

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-//Types
+// Types
 let sales = 123_456_789;
 let coutse = 'TypeScript';
 let is_published = true;
@@ -9,29 +9,41 @@ function render(document: any) {
 
 }
 
-//Arrays
+// Arrays
 let numbers = [1, 2, 3];
 numbers.forEach(n => n.toExponential);
 
-//Tuples
+//T uples
 let user: [number, string] = [20, 'Ulya'];
 user.push(1);
 
-//Enums
+// Enums
 // const small = 1;
 // const medium = 2;
 // const large = 3;
 
-//PascalCase
+// PascalCase
 const enum Size { Small = 1, Medium, Large }
 let mySize: Size = Size.Medium;
 console.log(mySize);
 
-//Function 
+// Function 
 function calculateTax(income: number, taxYear = 2023): number {
      if (taxYear < 2023)
         return income * 1.2;
     return income * 1.3;
 }
-
 calculateTax(10_000);
+
+// Object
+let employee: {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void;
+} = { 
+    id: 1, 
+    name: 'Ulya',
+    retire: (date: Date) => {
+        console.log(date);
+    }
+ };
